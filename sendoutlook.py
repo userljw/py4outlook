4269fc9dadb443c50ac5918c36499a1fc22ab2f6
LuJiawei <623079952@qq.com>
2018-01-12 11:44:42 +0800

    Update sendoutlook.py

--- sendoutlook.py
+++ sendoutlook.py
@@ -14,8 +14,8 @@ def outlook():
     app = 'Outlook'
     olook = win32.gencache.EnsureDispatch("%s.Application" % app)
     mail = olook.CreateItem(win32.constants.olMailItem)
-    mail.Recipients.Add('1@spdb.com.cn')
-    mail.CC='1@spdb.com.cn;2@spdb.com.cn'
+    mail.Recipients.Add('1@.com.cn')
+    mail.CC='1@.com.cn;2.com.cn'
     mail.Subject = unicode(' ' % today)
     body = unicode("！")
     mail.Body = body
